db1aab1eb8646e4a617e2178d61be651a7f98d4a
Duy Anh <duyanhdao297@gmail.com>
2024-09-15 00:38:50 +0700

    Setup backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(PORT, () => {
+   console.log("Server is running on http://localhost:" + PORT);
+});
