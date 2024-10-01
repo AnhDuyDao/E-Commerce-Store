6a63d0e4d48da5efe18d79375ea5a2c7a3fa54be
Duy Anh <duyanhdao297@gmail.com>
2024-10-01 23:05:41 +0700

    Backed login and refresh-token

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -54,7 +54,7 @@ userSchema.pre("save", async function (next) {
 });
 
 //
-userSchema.methods.comparePassword = async (password) => {
+userSchema.methods.comparePassword = async function (password) {
    return bcrypt.compare(password, this.password);
 };
 
