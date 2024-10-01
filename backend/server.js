ad2ed14b77a28708aed92b404c0f7973ce3e92ef
Duy Anh <duyanhdao297@gmail.com>
2024-10-01 16:48:00 +0700

    Backend signup session

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); //allows you to parse the body of request
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
