0ae16cc285a5c5281dd9318b51df59cc17b695f1
Duy Anh <duyanhdao297@gmail.com>
2024-10-06 15:47:23 +0700

    Createproduct route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
@@ -20,7 +21,7 @@ export const getFeaturedProducts = async (req, res) => {
 
       //if no featured products in redis, fetch from mongoDB
       //.lean() is gonna return a plain javascript object instead of a mongoose document
-      // which is good for performance
+      // which is good for per
       featuredProducts = await Product.find({ isFeatured: true }).lean();
 
       if (!featuredProducts) {
@@ -37,3 +38,27 @@ export const getFeaturedProducts = async (req, res) => {
       res.status(500).json({ message: "Server error", error: error.message });
    }
 };
+
+export const createProduct = async (req, res) => {
+   try {
+      const { name, description, price, image, category } = req.body;
+
+      let cloudinaryResponse = null;
+
+      if (image) {
+         cloudinaryResponse = await cloudinary.uploader.upload(image, { folder: "products" });
+      }
+
+      const product = await Product.create({
+         name,
+         description,
+         price,
+         image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+         category
+      });
+      res.status(201).json(product);
+   } catch (error) {
+      console.log("Error in createProduct controller", error.message);
+      res.status(500).json({ message: "Server error", error: error.message });
+   }
+}
