e363ebc6258a37d78cc9327bea79583bab770f98
hung <hungdeptraipc2004@gmail.com>
2024-10-06 08:41:24 +0700

    controllers branch

---
+++ backend/routes/product.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.router();
+
+
+router.get("/",getAllProducts)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 
@@ -15,8 +17,10 @@ app.use(cookieParser());
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
    console.log("Server is running on http://localhost:" + PORT);
+
    connectDB();
 });
